38405525d85f7bed77da2ab93a59010a0738aa59
bt33 <bt33@pc3-044-l.cs.st-andrews.ac.uk>
2016-04-05 16:42:02 +0100

    Added drawing of pieces on grid from world state

--- src/Draw.hs
+++ src/Draw.hs
@@ -1,6 +1,7 @@
 module Draw(drawWorld) where
 
 import Graphics.Gloss
+import Data.Monoid
 import Board
 
 -- Given a world state, return a Picture which will render the world state.
@@ -9,9 +10,10 @@ import Board
 -- This will need to extract the Board from the world state and draw it
 -- as a grid plus pieces.
 drawWorld :: World -> Picture
-drawWorld w = grid
+drawWorld w = result
  where
 
+  result = pictures [grid, mconcat boardPieces]
   darkGreen = makeColor 0.1 0.8 0.5 0.6
 
   grid = pictures
@@ -30,3 +32,16 @@ drawWorld w = grid
     color black (line [ (-320, 80), (320,  80) ]),
     color black (line [ (-320, 160), (320,  160) ]),
     color black (line [ (-320, 240), (320,  240) ])]
+
+  boardPieces = mconcat [map drawPiece (pieces (board w))]
+
+drawPiece :: (Position, Col) -> Picture
+drawPiece ((x,y), col) = piece
+
+ where
+  piece =
+   translate (-320.0 + (fromIntegral x) * 80.0 + 40.0)
+             (320 - (fromIntegral y) * 80.0 - 40.0) $
+        case col of
+          White -> color white (thickCircle 1 75)
+          Black -> color black (thickCircle 1 75)
